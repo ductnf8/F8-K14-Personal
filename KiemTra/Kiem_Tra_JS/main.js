c819cab1e4c53455de094193087274836aeccc8b
ductnf8 <ductnf8@fullstack.edu.vn>
2025-05-10 22:00:06 +0700

    Deadline

--- KiemTra/Kiem_Tra_JS/main.js
+++ KiemTra/Kiem_Tra_JS/main.js
@@ -59,9 +59,9 @@ Tim ra san phan ban chay nhat ( ban nhieu nhat ve bat so luong )
 
 console.log('Bai 4:')
 const productData = {}
-for(const order of orders) {
+for (const order of orders) {
     const orderCount = order.productId
-    if(!productData[orderCount]){
+    if (!productData[orderCount]) {
         productData[orderCount] = {
             id: order.productId,
             quantity: 0,
@@ -72,7 +72,7 @@ for(const order of orders) {
 // console.log(productData)
 const grouped4 = Object.values(productData)
 console.log(grouped4)
-const result4 =  grouped4.sort((a, b) => b.quantity - a.quantity)[0]
+const result4 = grouped4.sort((a, b) => b.quantity - a.quantity)[0]
 console.log(result4)
 /*
 
@@ -80,13 +80,13 @@ Bai 5:
 Tim ra san phan doanh thu cao nhat ( nhieu tien nhat )
 */
 console.log('Bai 5:')
-const group =[]
+const group = []
 products.forEach(product => {
     const a = grouped4.find(t => t.id === product.id)
     group.push({
         productId: product.id,
-        name:product.name,
-        sum: product.price*a.quantity,
+        name: product.name,
+        sum: product.price * a.quantity,
     })
 });
 const result5 = group.sort((a, b) => b.sum - a.sum)[0]
@@ -101,9 +101,9 @@ Tim ra nhan vien ban nhieu hang nhat
 
 console.log('Bài 6:')
 const ordersData = {}
-for(const order of orders) {
+for (const order of orders) {
     const orderCount = order.employeeId
-    if(!ordersData[orderCount]){
+    if (!ordersData[orderCount]) {
         ordersData[orderCount] = {
             employeeId: order.employeeId,
             quantity: 0,
@@ -113,7 +113,7 @@ for(const order of orders) {
 }
 const grouped6 = Object.values(ordersData)
 console.log(grouped6)
-const result6 =  grouped6.sort((a, b) => b.quantity - a.quantity)[0]
+const result6 = grouped6.sort((a, b) => b.quantity - a.quantity)[0]
 const result6a = employees.find((e) => e.id === result6.employeeId)
 console.log(result6a)
 
@@ -122,22 +122,42 @@ Bai 7:
 Tim ra nhan vien co doanh thu cao nhat
 */
 console.log('Bai 7:')
-const productData7 = {}
-for(const order of orders) {
-    const orderCount = order.employeeId
-    if(!productData7[orderCount]){
-        productData7[orderCount] = {
-            employeeId: order.employeeId,
-            productId: order.productId,
-            quantity: 0,
-        }
-    }
-    productData7[orderCount].quantity += order.quantity
-}
-console.log(productData7)
-
 const test7 = orders.sort((a, b) => a.employeeId - b.employeeId)
 console.log(test7)
+
+const group7 = []
+test7.forEach(test => {
+    const a = products.find(p => p.id === test.productId)
+    group7.push({
+        employeeId: test.employeeId,
+        productId: test.id,
+        // quantity: test.quantity,
+        // price: a.price,
+        sum: test.quantity * a.price
+    })
+});
+console.log(group7)
+
+const ordersData7 = {}
+for (const group of group7) {
+    const orderCount = group.employeeId
+    if (!ordersData7[orderCount]) {
+        ordersData7[orderCount] = {
+            employeeId: group.employeeId,
+            sum: 0,
+        }
+    }
+    ordersData7[orderCount].sum += group.sum
+}
+console.log(ordersData7)
+const grouped7 = Object.values(ordersData7)
+console.log(grouped7)
+const result7 = grouped7.sort((a, b) => b.sum - a.sum)[0]
+// console.log(result7)
+const result7a = employees.find((e) => e.id === result7.employeeId)
+console.log(result7a)
+
+
 /*
 
 
@@ -148,7 +168,17 @@ Tim ra san pham ban co doanh thu nhat cua moi nhan vien
 Bai 9:
 Gia su nhan vien se nhan duoc hoa hong la 3%
 -> tim hoa hong cho moi nhan vien
-
+*/
+// const group9 = []
+// employees.forEach(employee => {
+//     const a = group7.find(g => g.i === employee.productId)
+//     group9.push({
+//         ...employee,
+//         sum:
+//     })
+// });
+// console.log(group9)
+/*
 
 Bai 10:
 Sap xep nhan vien theo thu tu giam dan theo doanh thu
